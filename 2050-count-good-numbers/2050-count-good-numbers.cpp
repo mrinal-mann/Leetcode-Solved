7a9397290f4c1057099f4d96c951d55b69d69eba
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-12-13 20:50:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (6.74%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -3,18 +3,18 @@ class Solution {
 public:
     long long power(long long x,long long y){
         if(y==0) return 1;
-        long long ans=power(x,y/2);
+        long long ans=power(x,y/2); //recursion
         ans*=ans;
         ans%=mod;
-        if(y%2) ans*=x;
+        if(y%2) ans*=x; // multiply when y is odd
         ans%=mod;
         return ans; 
     }
 
 
     int countGoodNumbers(long long n) {
-        long long even=(n%2)+(n/2);
-        long long odd=n/2;
-        return (power(5,even)*power(4,odd))%mod;
+        long long even=(n%2)+(n/2); //for finding the even number of index
+        long long odd=n/2; //for finding the odd number of index
+        return (power(5,even)*power(4,odd))%mod; //created own power function
     }
 };
