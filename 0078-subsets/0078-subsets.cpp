699b18292c350010e48618aafd270c6620872008
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2024-12-14 14:24:49 +0530

    Time: 0 ms (100%), Space: 10 MB (44.74%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-
-    void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector<int>& subset){
-        if(index==nums.size()){
-            res.push_back(subset);
+    // Recursive function to generate subsets
+    void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector<int>& subset) {
+        // Base case: if we've processed all elements in the array
+        if (index == nums.size()) {
+            res.push_back(subset);  // Add the current subset to the result
             return;
         }
 
-        subset.push_back(nums[index]);
-        createSubset(nums,index+1,res,subset);
+        // **Include the current element in the subset**
+        subset.push_back(nums[index]);            // Add the current element
+        createSubset(nums, index + 1, res, subset);  // Recurse to process the next element
 
-        subset.pop_back();
-        createSubset(nums,index+1,res,subset);
+        // **Exclude the current element from the subset**
+        subset.pop_back();                        // Remove the current element
+        createSubset(nums, index + 1, res, subset);  // Recurse to process the next element
     }
+
+    // Function to generate all subsets of a given array
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> subset;
-        createSubset(nums,0,result,subset);
-        return result;
+        vector<vector<int>> result;  // To store all subsets
+        vector<int> subset;          // A temporary vector to store the current subset
+        createSubset(nums, 0, result, subset);  // Start recursion from the first element
+        return result;  // Return the list of all subsets
     }
 };
