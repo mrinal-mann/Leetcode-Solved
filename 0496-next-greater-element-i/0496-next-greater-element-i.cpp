25bb22bd77cd5294eefcd0d935f9e1c6e45d80a4
Mrinal Manna <118587617+mrinal-mann@users.noreply.github.com>
2025-01-17 20:26:14 +0530

    Time: 0 ms (100%), Space: 12.7 MB (42.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-      unordered_map<int,int> mpp;
-      stack<int> st;
+        unordered_map<int, int> mpp;
+        stack<int> st;
 
-      for(int i = nums2.size()-1 ; i>=0; i--){
-        
-// check if the stack holds any values and compare them with array values.   
-        while(!st.empty() && st.top() <= nums2[i]){
-// if the values in the stack are less than the array values delete them.
-        st.pop();
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            // check if the stack holds any values and compare them with array
+            // values.
+            while (!st.empty() && st.top() <= nums2[i]) {
+                // if the values in the stack are less than the array values
+                // delete them.
+                st.pop();
+            }
+            // if stack is not empty set the value to the top.
+            if (!st.empty())
+                mpp[nums2[i]] = st.top();
+            else
+                mpp[nums2[i]] = -1;
+            st.push(nums2[i]);
         }
-        // if stack is not empty set the value to the top.
-        if(!st.empty()) mpp[nums2[i]] = st.top();
-        else mpp[nums2[i]] = -1;
-        st.push(nums2[i]);
-      }
-      vector<int> ans;
-// check for array-1 values in map and store them.
-      for(int num:nums1){
-        ans.push_back(mpp[num]);
-      }
-      return ans;
+        vector<int> ans;
+        // check for array-1 values in map and store them.
+        for (int num : nums1) {
+            ans.push_back(mpp[num]);
+        }
+        return ans;
     }
-    
 };
